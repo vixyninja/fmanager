290f1288df3b75ff0be05e77e1590d4dbf7a9a1a
vixyninja <nevergiveup2k3@gmail.com>
2023-10-17 17:16:32 +0700

    feat: update notifications image

--- lib/core/firebase/firebase_binding.dart
+++ lib/core/firebase/firebase_binding.dart
@@ -1,12 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:fmanager/core/firebase/firebase.dart';
-import 'package:fmanager/firebase_options.dart';
 import 'package:get/get.dart';
 
 class FirebaseBinding extends Bindings {
   @override
   void dependencies() async {
-    Get.put<FirebaseApp>(await Firebase.initializeApp(options: DefaultFirebaseOptions.android));
     Get.put<FirebaseMessagingSer>(FirebaseMessagingSer());
   }
 }
