9f5d01ddf44a7cf50c9602a038df0f0804d5e74a
Synxndh <ndhs824@gmail.com>
2023-10-21 13:04:04 +0700

    feat: history

--- lib/views/manager/history/history/manager_history_view.dart
+++ lib/views/manager/history/history/manager_history_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fmanager/views/manager/history/history/manager_history_logic.dart';
+import 'package:fmanager/views/manager/history/history/utils/constants.dart';
+import 'package:fmanager/views/manager/history/history/widgets/card_history.dart';
 import 'package:get/get.dart';
 
 class ManagerHistoryView extends GetView<ManagerHistoryLogic> {
@@ -7,8 +9,38 @@ class ManagerHistoryView extends GetView<ManagerHistoryLogic> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('ManagerHistoryView')),
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) => Container(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: CardHistory(
+                      title: histories[index]['title'],
+                      creator: histories[index]['creator'],
+                      date: histories[index]['date'],
+                      hour: histories[index]['hour'],
+                      room: histories[index]['room'],
+                      image: histories[index]['image'],
+                      theme: theme,
+                    ),
+                  ),
+                  itemCount: histories.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
