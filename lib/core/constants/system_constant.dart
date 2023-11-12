e45818ebcbc0c906cf5352b81a9f4ae8dec4697e
vixyninja <nevergiveup2k3@gmail.com>
2023-11-12 12:15:38 +0700

    fix: default route, baseUrl

--- lib/core/constants/system_constant.dart
+++ lib/core/constants/system_constant.dart
@@ -1,5 +1,5 @@
 class SystemConstant {
   static const String appName = 'Office Management Pro Max';
 
-  static const String baseUrl = 'https://fm-gt5w.onrender.com/api/v1';
+  static const String baseUrl = 'http://192.168.1.5:7000/api/v1';
 }
